aba85a7d7ba906d2375d8d911663fe8e1d25bdc3
ShojiroAbe <astook89@gmail.com>
2021-04-17 16:23:59 +0900

    firestoreサービスのコンフィグ13:00、不要ファイルの削除

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+// firebase 内のサービスのエントリーポイント
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
+import "firebase/function"
+import {firebaseConfig} from "./config";
+
+firebase.initializeApp{firebaseConfig};
+//インポートしてきたメソッドの実行結果を定数にして使いやすいようにする
+export const auth = firebase.auth();
+export const db = firebase.firastore()
+export const storage = firebase.storage();
+export const functions = firebase.functions();
+export const FirebaseTimestamp = firebase.firestore.Timestamp;
